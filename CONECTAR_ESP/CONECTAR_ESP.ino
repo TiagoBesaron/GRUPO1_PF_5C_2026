05ccb45cccb4107c1b1b7730dccf07db1460e4ee
TiagoBesaron <49009246@est.ort.edu.ar>
2026-06-26 11:31:12 -0300

    Funciona

--- CONECTAR_ESP/CONECTAR_ESP.ino
+++ CONECTAR_ESP/CONECTAR_ESP.ino
@@ -1,9 +1,13 @@
-#include <esp_now.h>
 #include <WiFi.h>
+#include <esp_now.h>
 
-const uint8_t MAC_SENDER_1[] = { 0xE0, 0x72, 0xA1, 0x72, 0xDA, 0x54 };
+const uint8_t MAC_SENDER_1[] = {
+  0xE0, 0x72, 0xA1, 0x72, 0xDA, 0x54
+};
 
-const uint8_t MAC_RECEIVER_1[] = { 0xE0, 0x72, 0xA1, 0x72, 0xD8, 0xBC };
+const uint8_t MAC_RECEIVER_1[] = {
+  0xE0, 0x72, 0xA1, 0x72, 0xD8, 0xBC
+};
 
 const uint8_t* SENDERS_MACS[] = { MAC_RECEIVER_1 };
 const uint8_t SENDERS_COUNT = sizeof(SENDERS_MACS) / sizeof(uint8_t*);
@@ -11,56 +15,74 @@ const uint8_t SENDERS_COUNT = sizeof(SENDERS_MACS) / sizeof(uint8_t*);
 const uint8_t* RECEIVERS_MACS[] = { MAC_SENDER_1 };
 const uint8_t RECEIVERS_COUNT = sizeof(RECEIVERS_MACS) / sizeof(uint8_t*);
 
-void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
-	Serial.print("\r\nLast Packet Send Status:\t");
-	Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+void OnDataSent(const wifi_tx_info_t *info, esp_now_send_status_t status)
+{
+  Serial.print("Last Packet Send Status: ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void SendMessage() {
-	String payload = "Hola Eze";
+void SendMessage()
+{
+  String payload = "Hola Eze";
 
-	esp_err_t result = esp_now_send(MAC_RECEIVER_1, (uint8_t*)payload.c_str(), payload.length());
+  esp_err_t result = esp_now_send(
+    MAC_RECEIVER_1,
+    (uint8_t *)payload.c_str(),
+    payload.length()
+  );
 
-	if (result == ESP_OK) {
-		Serial.println("Sent with success");
-	} else {
-		Serial.println("Error sending the data");
-	}
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
 }
 
-void static RegisterPeeks() {
-	esp_now_peer_info_t peerInfo = {};
-	memcpy(peerInfo.peer_addr, MAC_RECEIVER_1, 6);
-	peerInfo.channel = 0;
-	peerInfo.encrypt = false;
+void RegisterPeeks()
+{
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, MAC_RECEIVER_1, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
 
-	if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-		Serial.println("Failed to add peer");
-	} else {
-		Serial.print("Registered peer ");
-	}
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+  }
+  else
+  {
+    Serial.println("Registered peer");
+  }
 }
 
-void static InitEspNow() {
-	if (esp_now_init() != ESP_OK) {
-		Serial.println("Error initializing ESP-NOW");
-	} else {
-		esp_now_register_send_cb(OnDataSent);
+void InitEspNow()
+{
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
 
-		RegisterPeeks();
-	}
+  esp_now_register_send_cb(OnDataSent);
+
+  RegisterPeeks();
 }
 
-void setup() {
-	Serial.begin(115200);
-	delay(2000);
-	WiFi.mode(WIFI_STA);
+void setup()
+{
+  Serial.begin(115200);
+  delay(2000);
 
-	InitEspNow();
+  WiFi.mode(WIFI_STA);
+
+  InitEspNow();
 }
 
-void loop() {
-	SendMessage();
-
-	delay(2000);
+void loop()
+{
+  SendMessage();
+  delay(2000);
 }
---
+++ ReceptorESP/ReceptorESP.ino
@@ -0,0 +1,71 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+
+const uint8_t MAC_SENDER_1[] = {
+  0xE0, 0x72, 0xA1, 0x72, 0xDA, 0x54
+};
+
+
+const uint8_t MAC_RECEIVER_1[] = {
+  0xE0, 0x72, 0xA1, 0x72, 0xD8, 0xBC
+};
+
+
+const uint8_t *SENDERS_MACS[] = { MAC_RECEIVER_1 };
+const uint8_t SENDERS_COUNT = sizeof(SENDERS_MACS) / sizeof(uint8_t *);
+
+
+const uint8_t *RECEIVERS_MACS[] = { MAC_SENDER_1 };
+const uint8_t RECEIVERS_COUNT = sizeof(RECEIVERS_MACS) / sizeof(uint8_t *);
+
+
+void OnMessageReceived(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
+  const uint8_t *mac = info->src_addr;
+
+
+  Serial.printf("Packet received from: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+
+  Serial.printf("Bytes received: %d\n", len);
+
+
+  String payload;
+  payload.reserve(len);
+
+
+  for (int i = 0; i < len; i++) {
+    payload += (char)data[i];
+  }
+
+
+  Serial.print("Mensaje: ");
+  Serial.println(payload);
+}
+
+
+void InitEspNow() {
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+
+  esp_now_register_recv_cb(OnMessageReceived);
+}
+
+
+void setup() {
+  Serial.begin(115200);
+
+
+  WiFi.mode(WIFI_STA);
+
+
+  InitEspNow();
+}
+
+
+void loop() {
+}
