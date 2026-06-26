b3d5a1a80adac3d1e9ed17e24e83030402371ac7
TiagoBesaron <49009246@est.ort.edu.ar>
2026-06-26 11:06:42 -0300

    Update CONECTAR_ESP.ino

--- CONECTAR_ESP/CONECTAR_ESP.ino
+++ CONECTAR_ESP/CONECTAR_ESP.ino
@@ -1,8 +1,9 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-const uint8_t MAC_SENDER_1[] = { E0:72:A1:72:DA:54 };
-const uint8_t MAC_RECEIVER_1[] = { E0:72:A1:72:D8:BC };
+const uint8_t MAC_SENDER_1[] = { 0xE0, 0x72, 0xA1, 0x72, 0xDA, 0x54 };
+
+const uint8_t MAC_RECEIVER_1[] = { 0xE0, 0x72, 0xA1, 0x72, 0xD8, 0xBC };
 
 const uint8_t* SENDERS_MACS[] = { MAC_RECEIVER_1 };
 const uint8_t SENDERS_COUNT = sizeof(SENDERS_MACS) / sizeof(uint8_t*);
@@ -10,61 +11,47 @@ const uint8_t SENDERS_COUNT = sizeof(SENDERS_MACS) / sizeof(uint8_t*);
 const uint8_t* RECEIVERS_MACS[] = { MAC_SENDER_1 };
 const uint8_t RECEIVERS_COUNT = sizeof(RECEIVERS_MACS) / sizeof(uint8_t*);
 
-void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status)
-{
+void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
 	Serial.print("\r\nLast Packet Send Status:\t");
 	Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void SendMessage()
-{
+void SendMessage() {
 	String payload = "Hola Eze";
 
 	esp_err_t result = esp_now_send(MAC_RECEIVER_1, (uint8_t*)payload.c_str(), payload.length());
 
-	if(result == ESP_OK)
-	{
+	if (result == ESP_OK) {
 		Serial.println("Sent with success");
-	}
-	else
-	{
+	} else {
 		Serial.println("Error sending the data");
 	}
 }
 
-void static RegisterPeeks()
-{
-	esp_now_peer_info_t peerInfo;
+void static RegisterPeeks() {
+	esp_now_peer_info_t peerInfo = {};
 	memcpy(peerInfo.peer_addr, MAC_RECEIVER_1, 6);
 	peerInfo.channel = 0;
 	peerInfo.encrypt = false;
 
-	if(esp_now_add_peer(&peerInfo) != ESP_OK)
-	{
+	if (esp_now_add_peer(&peerInfo) != ESP_OK) {
 		Serial.println("Failed to add peer");
-	}
-	else
-	{
+	} else {
 		Serial.print("Registered peer ");
 	}
 }
 
-void static InitEspNow()
-{
-	if(esp_now_init() != ESP_OK)
-	{
+void static InitEspNow() {
+	if (esp_now_init() != ESP_OK) {
 		Serial.println("Error initializing ESP-NOW");
-	}
-	else
-	{
+	} else {
 		esp_now_register_send_cb(OnDataSent);
-		
+
 		RegisterPeeks();
 	}
 }
 
-void setup()
-{
+void setup() {
 	Serial.begin(115200);
 	delay(2000);
 	WiFi.mode(WIFI_STA);
@@ -72,8 +59,7 @@ void setup()
 	InitEspNow();
 }
 
-void loop()
-{
+void loop() {
 	SendMessage();
 
 	delay(2000);
