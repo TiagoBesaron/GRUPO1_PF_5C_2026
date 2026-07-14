c0df015809e08644ff1112469c099cf25fbbe8c1
TiagoBesaron <49009246@est.ort.edu.ar>
2026-07-14 09:39:33 -0300

    Verificar Mac

--- ObtenerMACESP32/ObtenerMACESP32.ino
+++ ObtenerMACESP32/ObtenerMACESP32.ino
@@ -1,12 +1,40 @@
-#include "WiFi.h"
+#include <WiFi.h>
+
+unsigned long tiempoAnterior = 0;
+const unsigned long intervalo = 1000;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   WiFi.mode(WIFI_MODE_STA);
-  Serial.println(WiFi.macAddress());
+
+  while (!Serial) {
+    delay(10);
+  }
+
+  String mac = WiFi.macAddress();
+
+  Serial.println("Verificacion de la MAC");
+  Serial.print("MAC: ");
+  Serial.println(mac);
+
+  // Verificar longitud
+  if (mac.length() == 17) {
+    Serial.println("Longitud correcta");
+  } else {
+    Serial.println("Longitud incorrecta");
+  }
+
+  // Verificar fabricante
+  if (mac.startsWith("E0:72:A1")) {
+    Serial.println("La direccion MAC pertenece a Espressif Inc");
+  } else {
+    Serial.println("La direccion MAC no pertenece al bloque E0:72:A1");
+  }
 }
 
 void loop() {
-  delay(1000);
-  Serial.println(WiFi.macAddress());
+  if (millis() - tiempoAnterior >= intervalo) {
+    tiempoAnterior = millis();
+    Serial.println(WiFi.macAddress());
+  }
 }
